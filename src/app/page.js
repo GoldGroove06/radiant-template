06146c78ef3f81a3eaa57d90a6c8dbf58230dcdf
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-06-05 00:15:25 +0530

    intial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,49 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react';
+import Heading from '@radui/ui/Heading';
+import Text from '@radui/ui/Text';
+import Button from '@radui/ui/Button';  
+import Image from 'next/image';
 
-export default function Home() {
+function page(props) {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <div className='w-full h-screen bg-slate-900 rounded-3xl flex flex-col justify-center items-start space-y-4 pl-8'>
+          <p className="text-9xl font-semibold">Close every deal.</p>
+          <Text className="!text-2xl max-w-lg">
+            Radiant helps you sell more by revealing sensitive information about your customers.
+          </Text>
+          <div className='pt-8'>
+            <Button className="!bg-gray-1000 !text-slate-100 !rounded-3xl !p-4 !pt-5 !pb-5">
+              Get Started
+            </Button>
+            <Button className="!bg-gray-1000 !text-slate-100 !rounded-3xl !p-4 !pt-5 !pb-5">
+              See pricing 
+            </Button>
+          </div>
+      </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+
+      <section className='flex flex-row p-8 lg:text-5xl text-3xl justify-between flex-wrap'>
+        <div>logo</div>
+        <div>logo</div>
+        <div>logo</div>
+        <div>logo</div>
+        <div>logo</div>
+      </section>
+
+
+      <section className='max-w-[1240px] mx-auto p-8'>
+        <p className='text-4xl max-w-md'>A snapshot of your entire sales pipeline.</p>
+        <div className='p-2 bg-gray-100 rounded-3xl'>
+        <Image src="/images/app.png" alt="" height={500} width={1240} className='rounded-3xl border border-gray-400'/>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+
+      <section className='max-w-[1240px] mx-auto p-8'>
+        <p className='text-4xl max-w-md'>Know more about your customers than they do.</p>
+      </section>
     </div>
   );
 }
+
+export default page;
